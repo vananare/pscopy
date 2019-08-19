f0302bb7d6cf85eb3c62972dee63ce0b6d32f1c3
vananare <33262283+vananare@users.noreply.github.com>
2019-08-19 23:49:15 +0530

    initial commit

---
+++ Map_Copy.ps1
@@ -0,0 +1,59 @@
+﻿$Object =@() 
+$Computername=Get-Content("C:\temp\Compuetrs.txt")
+#get computers list here
+###get credentails######
+$creds_d = Get-Credential -Message "Enter crdetial to map drive with domain" 
+$Drive = "\\127.0.0.1\MyTest1"
+$Letter = "k"
+####---------------####
+
+
+
+$Scriptcode ={
+New-PSDrive -Name $Letter -PSProvider "FileSystem" -Root $Drive -Credential $creds_d -ErrorAction Stop;
+
+Start-Sleep 5;
+
+Copy-Item -Path "$Letter\*" -Destination "E:\GCTI\software\" -Recurse;
+
+start-sleep 5;
+
+Remove-PSDrive -Name $Letter;
+
+}
+
+Foreach($EachServer in $Computername){
+    Write-Host "Working on $EachServer"
+    if(Test-Connection -ComputerName $EachServer -Count 2 -Quiet)
+    {
+        try{
+        Invoke-Command -ComputerName $EachServer -ScriptBlock $Scriptcode -ErrorAction Stop
+        $dirSize = Get-ChildItem "\\$EachServer\E$\GCTI\Software" -recurse -force | select Length  |Measure-Object -Sum
+        $dirSize = $dirSize.sum/1MB
+        $op = "Copied - $dirSize"
+        }
+        catch [Exception] {
+         $op = $_.Exception.Message
+        }
+        
+        $Object = New-Object PSObject -Property @{
+                HostName = $EachServer
+                Status = "Pinging"
+                Files = $op
+            }
+
+        $Object |Export-Csv -Path "c:\temp\Output.csv" -Append -Force
+
+    }
+    Else{
+
+    $Object = New-Object PSObject -Property @{
+            HostName = $EachServer
+            Status = "Not Pinging"
+            Files = "Not copied"
+           }
+
+    $Object |Export-Csv -Path "c:\temp\Output.csv" -Append -Force
+
+    }
+}
---
+++ Map_Copy_psexec.ps1
@@ -0,0 +1,55 @@
+﻿$Object =@() 
+$Computername=Get-Content("C:\temp\Compuetrs.txt")
+#get computers list here
+###get credentails######
+$PSExec = "C:\temp\PSExec.exe"
+$user_d = Read-Host -Prompt "Enter username to map drive"
+$pswd_d = Read-Host -Prompt "Enter password to map drive"
+$Drive = "\\127.0.0.1\MyTest1"
+$Letter = "k"
+####---------------####
+
+
+Foreach($EachServer in $Computername){
+    Write-Host "Working on $EachServer"
+    if(Test-Connection -ComputerName $EachServer -Count 2 -Quiet)
+    {
+        try{
+
+       
+        Start-Process -Filepath $PSExec -ArgumentList "\\$EachServer -u $user_d -p $pswd_d net use N: $Drive" -Wait
+        Start-Process -Filepath $PSExec -ArgumentList "\\$EachServer -u $user_d -p $pswd_d xcopy p:\ E:\GCTI\Software /E" -Wait
+        #Start-Process -Filepath $PSExec -ArgumentList "\\$EachServer -u $user_d -p $pswd_d net use p: /delete" -Wait
+
+
+        ##get foldersize after copy
+        $dirSize = Get-ChildItem "\\$EachServer\E$\GCTI\Software" -recurse -force | select Length  |Measure-Object -Sum
+        $dirSize.sum = $dirSize.sum/1MB
+
+        $op = "Copied - $dirSize.sum"
+        }
+        catch [Exception] {
+         $op = $_.Exception.Message
+        }
+        
+        $Object = New-Object PSObject -Property @{
+                HostName = $EachServer
+                Status = "Pinging"
+                Files = $op
+            }
+
+        $Object |Export-Csv -Path "c:\temp\Output.csv" -Append -Force
+
+    }
+    Else{
+
+    $Object = New-Object PSObject -Property @{
+            HostName = $EachServer
+            Status = "Not Pinging"
+            Files = "Not copied"
+           }
+
+    $Object |Export-Csv -Path "c:\temp\Output.csv" -Append -Force
+
+    }
+}
